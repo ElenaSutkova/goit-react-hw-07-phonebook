6fa682afa53e34be914fd85022fec878239718f6
ElenaSutkova <134842923+ElenaSutkova@users.noreply.github.com>
2024-01-25 13:44:51 +0200

    comm2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,17 @@
- const App = () => {
+import ContactList from "./ContactList/ContactList";
+import Form from "./Form/Form";
+import Filter from "./Filter/Filter";
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <Form />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
-  );
-};
+  )
+}
 
 export default App;
